896d2f7296513c76cdc13cc8b2415f00573923fe
Patrick Gordon <patrickgordon42@yahoo.com>
2019-06-21 12:24:26 -0500

    added some styled components

--- village/src/index.js
+++ village/src/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
-import styled from 'styled-components';
-import { createGlobalStyle } from 'styled-components';
+
+
 
 
 import './index.css';
 import App from './App';
 
-// Global Styles
 
-const GlobalStyle = createGlobalStyle`
-    body {
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-    background-color: #88CCFF;
-    color: #FFF;
-}
-`
+
+
 
 
 
